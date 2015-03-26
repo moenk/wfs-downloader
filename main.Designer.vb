07d4c4027ba462114a35bf0285d2985a518d3c47
Thomas "moenk" Mönkemeier <moenk@geoclub.de>
2015-03-26 09:41:55 +0100

    init

---
+++ main.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(526, 100)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Get Layers"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(487, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "C:\Program Files\GDAL"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Path to GDAL (required!)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(188, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "WFS Server URL (without parameters)"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 100)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(487, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "http://maps.geo.hu-berlin.de/geoserver/wfs"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(18, 165)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(586, 147)
+        Me.ListBox1.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(18, 354)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(481, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(526, 352)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Select"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 338)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Output Directory"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 149)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Layers"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(18, 393)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Download"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(526, 33)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Change"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(526, 393)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "Exit"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(624, 442)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "main"
+        Me.Text = "WFS Downloader v0.2 by moenk"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ main.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class main
+
+    Dim PathToGDAL As String
+    Dim DriveOfGDAL As String
+    Dim WFS_URL As String
+    Dim ShapeOutput As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Zeile As String
+        Dim LayerArray() As String
+        If My.Computer.FileSystem.FileExists(PathToGDAL + "\ogrinfo.exe") Then
+            ListBox1.Items.Clear()
+            Dim proc As ProcessStartInfo = New ProcessStartInfo("cmd.exe")
+            Dim pr As Process
+            Cursor.Current = Cursors.WaitCursor
+            proc.CreateNoWindow = True
+            proc.UseShellExecute = False
+            proc.RedirectStandardInput = True
+            proc.RedirectStandardOutput = True
+            pr = Process.Start(proc)
+            DriveOfGDAL = Microsoft.VisualBasic.Left(PathToGDAL, 2)
+            pr.StandardInput.WriteLine(DriveOfGDAL)
+            pr.StandardInput.WriteLine("cd """ + PathToGDAL + """")
+            pr.StandardInput.WriteLine("ogrinfo.exe -ro -so -q WFS:""" + WFS_URL + """")
+            pr.StandardInput.Close()
+            Do Until pr.StandardOutput.EndOfStream()
+                Zeile = pr.StandardOutput.ReadLine()
+                LayerArray = Split(Zeile, " ")
+                If Val(LayerArray(0)) > 0 Then
+                    ListBox1.Items.Add(LayerArray(1))
+                    REM ListBox1.Refresh()
+                End If
+            Loop
+            pr.StandardOutput.Close()
+            Cursor.Current = Cursors.Default
+        Else
+            Dim response = MsgBox("GDAL not found. Download now?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Confirm")
+            If response = MsgBoxResult.Yes Then
+                Process.Start("http://download.gisinternals.com/sdk/downloads/release-1800-x64-gdal-1-11-1-mapserver-6-4-1/gdal-111-1800-x64-core.msi")
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        PathToGDAL = Trim(TextBox1.Text)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        WFS_URL = Trim(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox3.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If ListBox1.SelectedIndex >= 0 Then
+            Dim LayerSelected As String = ListBox1.Items(ListBox1.SelectedIndex).ToString
+            If ShapeOutput <> "" And LayerSelected <> "" Then
+                Dim DriveOfShape = Microsoft.VisualBasic.Left(ShapeOutput, 2)
+                Dim ShapeLayer As String
+                If My.Computer.FileSystem.FileExists(PathToGDAL + "\ogr2ogr.exe") Then
+                    Dim proc As ProcessStartInfo = New ProcessStartInfo("cmd.exe")
+                    Dim pr As Process
+                    Cursor.Current = Cursors.WaitCursor
+                    proc.CreateNoWindow = True
+                    proc.UseShellExecute = False
+                    proc.RedirectStandardInput = True
+                    proc.RedirectStandardOutput = True
+                    pr = Process.Start(proc)
+                    pr.StandardInput.WriteLine(DriveOfShape)
+                    pr.StandardInput.WriteLine("cd """ + ShapeOutput + """")
+                    pr.StandardInput.WriteLine("set GDAL_DATA=" + PathToGDAL + "\gdal-data\")
+                    ShapeLayer = Regex.Replace(LayerSelected, "[^\w]", "_")
+                    Dim OGRBefehl As String = """" + PathToGDAL + "\ogr2ogr.exe"" -overwrite -f ""ESRI Shapefile"" """ + ShapeLayer + """ -nln """ + ShapeLayer + """ WFS:" + WFS_URL + " """ + LayerSelected + """"
+                    pr.StandardInput.WriteLine(OGRBefehl)
+                    pr.StandardInput.Close()
+                    Console.WriteLine(pr.StandardOutput.ReadToEnd())
+                    pr.StandardOutput.Close()
+                    Cursor.Current = Cursors.Default
+                    REM TextBox1.Text = OGRBefehl
+                    Process.Start("explorer.exe", ShapeOutput)
+                Else
+                    MsgBox("GDAL not found.")
+                End If
+            End If
+        Else
+            MsgBox("No layer selected.")
+        End If
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+        ShapeOutput = Trim(TextBox3.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Dim response As MsgBoxResult
+        response = MsgBox("Do you want to exit?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Confirm")
+        If response = MsgBoxResult.Yes Then
+            Me.Dispose()
+        ElseIf response = MsgBoxResult.No Then
+            Exit Sub
+        End If
+    End Sub
+End Class
